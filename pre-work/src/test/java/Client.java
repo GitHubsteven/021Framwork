2df6758d837788b5ced479ac7ca9dfbade74edee
rongbin.xie <xrb16302@163.com>
2018-06-12 09:11:58 +0800

    + basic aop advice

--- pre-work/src/test/java/Client.java
+++ pre-work/src/test/java/Client.java
@@ -1,10 +1,17 @@
+import aop.advice.GreetingAfterAdvice;
+import aop.advice.GreetingBeforeAdvice;
 import org.springframework.aop.framework.ProxyFactory;
+import service.greet.IGreetService;
 import service.greet.IGreetServiceImpl;
 
 public class Client {
     public static void main(String[] args) {
         ProxyFactory proxyFactory = new ProxyFactory();
         proxyFactory.setTarget(new IGreetServiceImpl());
-        proxyFactory.getProxy();
+        proxyFactory.addAdvice(new GreetingBeforeAdvice());
+        proxyFactory.addAdvice(new GreetingAfterAdvice());
+
+        IGreetService proxy = (IGreetService) proxyFactory.getProxy();
+        proxy.greet("jack");
     }
 }
