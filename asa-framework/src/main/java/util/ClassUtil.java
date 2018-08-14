a9bca21c1c339c5f105d876f5f1d2a32e9d4b7cb
rongbin.xie <xrb16302@163.com>
2018-08-15 00:14:22 +0800

    + class util

--- asa-framework/src/main/java/util/ClassUtil.java
+++ asa-framework/src/main/java/util/ClassUtil.java
@@ -1,13 +1,21 @@
 package util;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.net.JarURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Created with IntelliJ IDEA.
@@ -66,17 +74,69 @@ public class ClassUtil {
                 URL url = urls.nextElement();
                 if (url != null) {
                     String protocol = url.getProtocol();
-                    url.getPath().replaceAll("%20", " ");
+                    if (protocol.equals("file")) {
+                        //如果是文件的话，那么进行文件中的文件扫描，调用addClass函数
+                        String packagePath = url.getPath().replaceAll("%20", "");
+                        addClass(classSet, packagePath, packageName);
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            Enumeration<JarEntry> entries = jarFile.entries();
+                            while (entries.hasMoreElements()) {
+                                JarEntry jarEntry = entries.nextElement();
+                                String jarEntryName = jarEntry.getName();
+                                if (jarEntryName.endsWith(".class")) {
+                                    String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/", ".");
+                                    doAddClass(classSet, className);
+                                }
+                            }
+                        }
 
+                    }
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("get class set failure", e);
+            throw new RuntimeException(e);
         }
+        return classSet;
     }
 
     private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
-        
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                //或者class文件或者是文件夹
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+        if (files == null || files.length == 0) return;
+        for (File file : files) {
+            String fileName = file.getName();
+            if (file.isFile()) {
+                //对文件进行加载
+                String className = fileName.substring(0, fileName.lastIndexOf("."));
+                if (packageName != null) {
+                    className = packageName + className;
+                }
+                doAddClass(classSet, className);
+            } else {
+                //如果是文件夹的话，package Path + packageName,然后文件名称变为包名，递归
+                if (StringUtils.isNotEmpty(packagePath)) {
+                    packagePath = packagePath + "/" + packageName;
+                }
+                if (StringUtils.isNotEmpty(packageName)) {
+                    packageName = packageName + "." + fileName;
+                }
+                addClass(classSet, packagePath, packageName);
+            }
+        }
+    }
+
+    public static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className);
+        classSet.add(cls);
     }
 
 
